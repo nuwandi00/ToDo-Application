1042861128c1ac51a435425c4286d760cebb257d
nuwandihewamadduma <160816359+nuwandihewamadduma@users.noreply.github.com>
2024-05-20 10:48:13 +0530

    initial commit

---
+++ todolist/src/Create.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Create() {
+  return (
+    <div>
+
+        <input type="text" name="" id="" />
+        <button type="button">Add</button>
+    </div>
+  )
+}
+
+export default Create
